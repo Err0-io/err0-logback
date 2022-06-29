09402471591b21e61bdfd2f0ee913a6c880f810d
Ben Gould <ben@amb1ent.org>
2022-06-29 15:55:51 +0100

    Improve local testing for logback appender, with a configuration file outside the build src/.../resources.

--- src/main/java/io/err0/logback/Main.java
+++ src/main/java/io/err0/logback/Main.java
@@ -3,7 +3,17 @@ package io.err0.logback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This class exists only to test the software against a local instance
+ * of err0server.
+ */
 public class Main {
+
+    static
+    {
+        // for test, load configuration from the file in the project directory
+        System.setProperty("logback.configurationFile", "logback.xml");
+    }
 
     public static void main(String[] args) {
         try {
