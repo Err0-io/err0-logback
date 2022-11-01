25172643313a2243b5f38324db2b8859f1467b50
Ben Gould <ben@amb1ent.org>
2022-11-01 09:19:14 +0100

    convert httpclient -> okhttp publish to sonatype staging repository version number bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,22 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
-  java
-  application
+  `java-library`
   `maven-publish`
+  signing
 }
 
 group = "io.err0.logback"
 group = "io.err0.logback"
-version = "1.0.1-LOGCONTEXT"
+version = "1.0.2"
 
 repositories {
   mavenCentral()
 }
 
-publishing {
-  publications {
-    create<MavenPublication>("mavenJava") {
-      artifactId = "err0-logback"
-      from(components["java"])
-      versionMapping {
-        usage("java-api") {
-          fromResolutionOf("runtimeClasspath")
-        }
-        usage("java-runtime") {
-          fromResolutionResult()
-        }
-      }
-      pom {
-        name.set("err0-logback")
-        description.set("Logback adapter for err0")
-      }
-    }
-  }
-  repositories {
-    maven {
-      url = uri("https://gitlab.bluetrail.software/api/v4/projects/" + System.getenv("CI_PROJECT_ID") + "/packages/maven")
-      name = "BTS-GitLab"
-      credentials(HttpHeaderCredentials::class) {
-        name = "Job-Token"
-        value = System.getenv("CI_JOB_TOKEN")
-      }
-      authentication {
-        create<HttpHeaderAuthentication>("header")
-      }
-    }
-  }
-}
-
 val junitJupiterVersion = "5.7.0"
 
 val launcherClassName = "io.err0.logback.Main"
-
-application {
-  mainClass.set(launcherClassName)
-}
 
 dependencies {
   //implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
@@ -62,8 +24,8 @@ dependencies {
   //testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 
-  // https://mvnrepository.com/artifact/org.apache.httpcomponents.client5/httpclient5
-  implementation("org.apache.httpcomponents.client5:httpclient5:5.1")
+  // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+  implementation("com.squareup.okhttp3:okhttp:4.10.0")
   implementation("com.google.code.gson:gson:2.8.7")
 
   // the below are uncommented to allow me to use autocomplete to edit the
@@ -77,18 +39,83 @@ dependencies {
   // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
   implementation("ch.qos.logback:logback-classic:1.2.11")
 
+  // to silence library included slf4j noises:
+  //
   // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
   implementation("org.slf4j:slf4j-api:1.7.36")
+  // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+  compileOnly("org.slf4j:slf4j-simple:1.7.36")
 }
 
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
+  withJavadocJar()
+  withSourcesJar()
 }
 
 tasks.withType<Test> {
   useJUnitPlatform()
   testLogging {
     events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("mavenJava") {
+      from(components["java"])
+      pom {
+        name.set("err0-logback")
+        description.set("Connector for err0, logback")
+        url.set("https://github.com/Err0-io/err0-logback")
+        licenses {
+          license {
+            name.set("MIT license")
+            url.set("https://github.com/Err0-io/err0-logback/blob/main/LICENSE.txt")
+          }
+        }
+        developers {
+          developer {
+            id.set("err0package")
+            name.set("Err0.io packages")
+            email.set("package@err0.io")
+          }
+        }
+        versionMapping {
+          //usage("java-api") {
+          //  fromResolutionOf("runtimeClasspath")
+          //}
+          usage("java-runtime") {
+            fromResolutionResult()
+          }
+        }
+        scm {
+          connection.set("scm:git:https://github.com/Err0-io/err0-logback.git")
+          developerConnection.set("scm:git:ssh://git@github.com:Err0-io/err0-logback.git")
+          url.set("https://github.com/Err0-io/err0-logback")
+        }
+      }
+    }
+  }
+  repositories {
+    maven {
+      name = "sonatype"
+      setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+      credentials {
+        username = project.property("sonatypeUsername") as String?
+        password = project.property("sonatypePassword") as String?
+      }
+    }
+  }
+}
+
+signing {
+  sign(publishing.publications)
+}
+
+tasks.javadoc {
+  if (JavaVersion.current().isJava9Compatible) {
+    (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
   }
 }
